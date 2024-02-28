23a9a64030b18897bae7cf062e7965f16ce622de
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-02-28 18:16:33 +0200

    Change projects Name

--- Bulky.DataAccess/Repository/CategoryRepository.cs
+++ Bulky.DataAccess/Repository/CategoryRepository.cs
@@ -1,13 +1,13 @@
-﻿using Bulky.DataAccess.Data;
-using Bulky.DataAccess.Repository.IRepository;
-using Bulky.Models;
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Bulky.DataAccess.Repository
+namespace BulkyBook.DataAccess.Repository
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
@@ -17,10 +17,6 @@ namespace Bulky.DataAccess.Repository
         public void Update(Category obj)
         {
             _db.Categories.Update(obj);
-        }
-        public void Save()
-        {
-            _db.SaveChanges();
         }
     }
 }
