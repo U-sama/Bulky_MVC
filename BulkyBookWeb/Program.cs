55bf92e48c5979f2e7402931a2b4c86d1e7441cc
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-03-07 00:46:50 +0200

    Make some edits for identity to work also add migrations and update the Database

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -12,7 +12,9 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
@@ -31,8 +33,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// authenticate the user first
+app.UseAuthentication();
 app.UseAuthorization();
-
+app.MapRazorPages();
 app.MapControllerRoute(
     name: "default",
     pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
