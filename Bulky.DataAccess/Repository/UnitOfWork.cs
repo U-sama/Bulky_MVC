2079519591d04f813c7e0e5827a064570917a827
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-03-26 23:58:55 +0200

    Add reposetories for Order header & Detail

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -12,12 +12,11 @@ namespace BulkyBook.DataAccess.Repository
     {
         public ICategoryRepository Category { get; private set; }
         public IProductRepository Product { get; private set; }
-
         public ICompanyRepository Company { get; private set; }
-
         public IShoppingCartRepository ShoppingCart { get; private set; }
-
         public IApplicationUserRepository ApplicationUser { get; private set; }
+        public IOrderHeaderRepository OrderHeader { get; private set; }
+        public IOrderDetailRepository OrderDetail { get; private set; }
 
         private ApplicationDbContext _db;
         public UnitOfWork(ApplicationDbContext db)
@@ -28,6 +27,8 @@ namespace BulkyBook.DataAccess.Repository
             Company = new CompanyRepository(_db);
             ShoppingCart = new ShoppingCartRepository(_db);
             ApplicationUser = new ApplicationUserRepository(_db);
+            OrderHeader = new OrderHeaderRepository(_db);
+            OrderDetail = new OrderDetailRepository(_db);
         }
 
         public void Save()
