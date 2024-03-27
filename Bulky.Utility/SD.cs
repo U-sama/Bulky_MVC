f92bf74d62e58eaa448b7c24b671276ec5ffaec8
Osama Muhamed <63379624+U-sama@users.noreply.github.com>
2024-03-28 01:29:14 +0200

    add order logic for company user

--- Bulky.Utility/SD.cs
+++ Bulky.Utility/SD.cs
@@ -12,5 +12,21 @@ namespace BulkyBook.Utility
         public const string Role_Admin = "Admin";
         public const string Role_Employee = "Employee";
         public const string Role_Company = "Company";
-    }
+
+        // Order status
+        public const string StatusPanding = "Panding";
+        public const string StatusApproved = "Approved";
+        public const string StatusProcessing = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+		// Payment Status
+		public const string PaymentStatusPanding = "Panding";
+		public const string PaymentStatusApproved = "Approved";
+		public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+		public const string PaymentStatusRejected = "Rejected";
+
+
+	}
 }
--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyBook.DataAccess.Repository.IRepository;
 using BulkyBook.Models;
 using BulkyBook.Models.ViewModels;
+using BulkyBook.Utility;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
@@ -11,6 +12,7 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
@@ -65,7 +67,67 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        public IActionResult Plus (int cartId)
+        [HttpPost]
+        [ActionName("Summary")]
+		public IActionResult SummaryPost()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM.ShoppingCartsList = _unitOfWork.ShoppingCart.GetAll(c => c.ApplicationUserId == userId, IncludeProperties: "Product");
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+			ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+
+			foreach (var cart in ShoppingCartVM.ShoppingCartsList)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart);
+				ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+			}
+
+            if(applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                // Is a regular user 
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPanding;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPanding;
+            }
+            else
+            {
+                // Company user 
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+            }
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+
+            foreach (var cart in ShoppingCartVM.ShoppingCartsList)
+            {
+                OrderDetail orderDetail = new OrderDetail()
+                {
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    ProductId = cart.ProductId,
+                    Count = cart.Count,
+                    Price = cart.Price
+				};
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+            }
+
+			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+			{
+				// Is a regular user and we need to capture payment
+                // stripe logic
+			}
+
+			return RedirectToAction(nameof(OrderConfirmation), new { orderId = ShoppingCartVM.OrderHeader.Id} );
+		}
+
+        public IActionResult OrderConfirmation (int orderId)
+        {
+            return View(orderId);
+        }
+
+		public IActionResult Plus (int cartId)
         {
             ShoppingCart cart = _unitOfWork.ShoppingCart.Get(c => c.Id == cartId);
             cart.Count += 1;
